45c9b1b13fb6430cc3a4f719b3e7d907bd8acc30
hungxuan <nguyen.xuan.hung@framgia.com>
2018-05-04 15:34:07 +0700

    Finish toy app

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+  belongs_to :microposts
+  validates FILL_IN, presence: true    # Replace FILL_IN with the right code.
+  validates FILL_IN, presence: true    # Replace FILL_IN with the right code.
+end
